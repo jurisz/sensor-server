69ea6fb8b628e63d68992dff9c8552839b62258e
juris.zubkans <juris.zubkans@gmail.com>
2017-06-05 10:23:39 +0300

    project sketch

---
+++ src/main/kotlin/org/juz/ss/BootApplication.kt
@@ -0,0 +1,12 @@
+package org.juz.ss
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+open class BootApplication
+
+fun main(args: Array<String>) {
+	SpringApplication.run(BootApplication::class.java, *args)
+}
